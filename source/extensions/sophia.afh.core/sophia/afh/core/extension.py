8b2c1728535af0955f7d1bf83fd67ac15b040f53
sophiasupruniuk <sophia.supruniuk@gmail.com>
2026-08-04 16:28:11 -0600

    R4.1: split extension into modules with SimulationController

--- source/extensions/sophia.afh.core/sophia/afh/core/extension.py
+++ source/extensions/sophia.afh.core/sophia/afh/core/extension.py
@@ -10,52 +10,25 @@
 
 import omni.ext
 import omni.ui as ui
-
-
-# Functions and vars are available to other extensions as usual in python:
-# `sophia.afh.core.some_public_function(x)`
-def some_public_function(x: int):
-    """This is a public function that can be called from other extensions."""
-    print(f"[sophia.afh.core] some_public_function was called with {x}")
-    return x ** x
+from .simulation import SimulationController
 
 
 # Any class derived from `omni.ext.IExt` in the top level module (defined in
 # `python.modules` of `extension.toml`) will be instantiated when the extension
 # gets enabled, and `on_startup(ext_id)` will be called. Later when the
 # extension gets disabled on_shutdown() is called.
-class MyExtension(omni.ext.IExt):
-    """This extension manages a simple counter UI."""
+class SimulationExtension(omni.ext.IExt):
+    """Entry point for the Adaptive Fulfillment Hall simulation extension."""
     # ext_id is the current extension id. It can be used with the extension
     # manager to query additional information, like where this extension is
     # located on the filesystem.
     def on_startup(self, _ext_id):
         """This is called every time the extension is activated."""
         print("[sophia.afh.core] Extension startup")
-
-        self._count = 0
-        self._window = ui.Window(
-            "Adaptive Fulfillment Hall Core", width=300, height=300
-        )
-        with self._window.frame:
-            with ui.VStack():
-                label = ui.Label("")
-
-                def on_click():
-                    self._count += 1
-                    label.text = f"count: {self._count}"
-
-                def on_reset():
-                    self._count = 0
-                    label.text = "empty"
-
-                on_reset()
-
-                with ui.HStack():
-                    ui.Button("Add", clicked_fn=on_click)
-                    ui.Button("Reset", clicked_fn=on_reset)
+        self._simulation_controller = SimulationController()
 
     def on_shutdown(self):
         """This is called every time the extension is deactivated. It is used
         to clean up the extension state."""
         print("[sophia.afh.core] Extension shutdown")
+        self._simulation_controller = None
---
+++ source/extensions/sophia.afh.core/sophia/afh/core/simulation.py
@@ -0,0 +1,9 @@
+class SimulationController:
+    def __init__(self):
+        print("SimulationController created")
+
+    def start(self):
+        print("start")
+
+    def stop(self):
+        print("stop")
