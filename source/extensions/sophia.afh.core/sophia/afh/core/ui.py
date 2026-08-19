8ae36ca10821fe7faef61493436b987007558525
sophiasupruniuk <sophia.supruniuk@gmail.com>
2026-08-18 20:47:29 -0600

    R4.2: add Stop button

--- source/extensions/sophia.afh.core/sophia/afh/core/ui.py
+++ source/extensions/sophia.afh.core/sophia/afh/core/ui.py
@@ -5,13 +5,15 @@ class ControlPanel:
 
     def __init__(self, controller):
         self._controller = controller
-        def on_click():
+        def on_run():
             self._controller.start()
+        def on_stop():
+            self._controller.stop()
         self._window = ui.Window("Adaptive Fulfillment Hall", width=300, height=300)
         with self._window.frame:
-
             with ui.VStack():
-                ui.Button("Run", clicked_fn=on_click)
+                ui.Button("Run", clicked_fn=on_run)
+                ui.Button("Stop", clicked_fn=on_stop)
 
     def destroy(self):
         """Destroys the Control Panel window."""
